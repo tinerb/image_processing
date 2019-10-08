86677044923723289371afc2603902bbfde03867
iLooL <Ruckas_x1@hotmail.com>
2019-10-08 17:57:27 -0400

    Add files via upload

---
+++ segmentation.h
@@ -0,0 +1,31 @@
+/*
+ * segmentation.h
+ *
+ *  Created on: Oct. 1, 2019
+ *      Author: Adam Gumieniak
+ */
+
+#ifndef IMAGE_PROCESSING_SRC_SEGMENTATION_H_
+#define IMAGE_PROCESSING_SRC_SEGMENTATION_H_
+
+#include "image.h"
+#include "filter.h"
+
+
+typedef struct lnode {
+	int value;
+	int equivalent;
+	struct lnode *next;
+} equivalence_list;
+
+
+
+image label_image(image filtered_image);
+image apply_equivalence_list(image labeled_image, int equivalence_list);
+image raster_scan(image filtered_image);
+image apply_equivalence_list(image labeled_image, int equivalence_list);
+image segment_image(image filtered_image);
+
+
+
+#endif /* IMAGE_PROCESSING_SRC_SEGMENTATION_H_ */
