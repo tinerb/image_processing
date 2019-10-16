5108851beb2991a43449dd5516914eca4d171c39
iLooL <Ruckas_x1@hotmail.com>
2019-10-16 19:10:33 -0400

    Add files via upload

--- src/segmentation.h
+++ src/segmentation.h
@@ -17,12 +17,14 @@ typedef struct node {
 
 #include "image.h"
 #include "filter.h"
+#include "histogram.h"
 
-node *new_node(char data);
+node *new_node(float data, float equivalent);
 void insert_end(node **headp, node **tailp, node *new_np);
 void display_forward(node *head);
 void display_backward(node *tail);
 void clean(node **headp);
+node* search(node *tail, int num);
 image label_image(image filtered_image);
 image apply_equivalence_list(image labeled_image, int equivalence_list);
 image raster_scan(image filtered_image);
