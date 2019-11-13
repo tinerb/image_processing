9ca4ae356f7e2c59e1895a3f34dfeee4d503aab9
iLooL <Ruckas_x1@hotmail.com>
2019-11-13 16:05:08 -0500

    Add files via upload

--- src/segmentation.c
+++ src/segmentation.c
@@ -34,25 +34,36 @@ void display_forward(node *head) {
 	}
 }
 
-void display_backward(node *tail) {
+int len(node *head) {
+	int len = 0;
 	node *np;
-	for (np = tail; np != NULL; np = np->prev){
-		printf("%.2f, %.2f\n", np->data, np->equivalent);
+	for (np = head; np != NULL; np = np->next){
+		len++;
 	}
+
+	return len;
 }
 
+float get_eq_value_at_index(node *head, float num){
+	while ((head != NULL) && (head->data != num)) {
+		head = head->next;
 
-//// returns the node that we want to change
-//node* search(node *tail, int num, int equivalent) {
-//	while ((tail != NULL) && (tail->data != num)) {
-//		tail = tail->prev;
-//	}
-//
-//	tail->equivalent = equivalent;
-//	return tail;
-//}
+	}
+
+	return head->equivalent;
+}
 
 // returns the node that we want to change
+node* search(node *head, int num) {
+	while ((head != NULL) && (head->data != num)) {
+		head = head->next;
+	}
+
+	return head;
+}
+
+// when we find a pixel with set top and left values
+// we set that pixel to its lowest value
 void update_list(node *tail, int num, int equivalent) {
 	while ((tail != NULL) && (tail->data != num)) {
 		tail = tail->prev;
@@ -60,6 +71,40 @@ void update_list(node *tail, int num, int equivalent) {
 
 	tail->equivalent = equivalent;
 	return;
+}
+
+int is_unique(node *head, int num){
+	int unique = 0;
+	while (head != NULL) {
+		if(head->equivalent == num){
+			// number is not unique
+			unique = 1;
+		}
+		head = head->next;
+	}
+
+	return unique;
+}
+
+
+// LOOPING THROUGH ONCE IS NOT ENOUGH; MAKE IT SO THAT WE KNOW FOR SURE EACH VALUE HAS BEEN CHANGED
+int get_regions(node *temp){
+	int regions = 1;
+	node *temp_head=NULL, *temp_tail=NULL;
+	node *n = new_node(temp->data, temp->equivalent);
+	insert_end(&temp_head, &temp_tail, n); // insert to start the list which avoids errors when we search for unique
+	temp = temp->next;
+	while (temp != NULL) {
+		if(is_unique(temp_head, temp->equivalent) == 0){
+			node *n = new_node(temp->data, temp->equivalent);
+			insert_end(&temp_head, &temp_tail, n);
+			printf("temp->data = %f -- temp->equivalent = %f\n",temp->data, temp->equivalent);
+			regions++;
+		}
+		temp = temp->next;
+	}
+
+	return regions;
 }
 
 void clean(node **headp) {
@@ -78,12 +123,31 @@ void clean(node **headp) {
  * foreground(black pixels) as -1 and background(white pixels)
  * as 0
  *
- * returns result
+ * returns (image) result
 */
 image label_image(image filtered_image){
 	image result = make_image(filtered_image.w, filtered_image.h, 1);
 	int black = 0, white = 0;
 	float threshold = histogram_threshold(filtered_image);
+	for (int i = 0; i < result.w * result.h; i++){
+		if (filtered_image.data[i] < threshold){
+			result.data[i] = 0; // black pixel
+			white++;
+		}
+		else{
+			result.data[i] = 1; // white pixel
+			black++;
+		}
+	}
+
+	return result;
+}
+
+
+// binary image still has unseen
+image trivial_clamp(image filtered_image, float threshold){
+	image result = make_image(filtered_image.w, filtered_image.h, 1);
+	int black = 0, white = 0;
 	for (int i = 0; i < result.w * result.h; i++){
 		if (filtered_image.data[i] < threshold){
 			result.data[i] = 0; // black pixel
@@ -105,17 +169,25 @@ image label_image(image filtered_image){
  * returns labeled_image
  */
 image raster_scan(image filtered_image, int is_binary){
-	// our current equivalence label number
+	// our current equivalence label number; start at 2 so we dont change pixel values of 1 (white)
 	int current_equivalence = 2;
+	int list_len;
+	float node_data, node_eq; // used for when we correct
 	float top_pixel, left_pixel, current_pixel, top_left_pixel;
 	image labeled_image;
+	node *temp = NULL;
 
 	if(is_binary == 1){
 		labeled_image = label_image(filtered_image);
 	}
 	else{
-		labeled_image = filtered_image;
+		// binary images still have strange values which are not 0 or 1
+		// clamp using some arbitrary value to enforce binary pixels
+		// change threshold depending on image you use
+		labeled_image = trivial_clamp(filtered_image, 0.9); // change threshold value depending on image you use
 	}
+
+//	return labeled_image;
 
 	for(int y = 0; y < (labeled_image.h); y++){
 		for(int x = 0; x < (labeled_image.w); x++){
@@ -129,15 +201,14 @@ image raster_scan(image filtered_image, int is_binary){
 				top_left_pixel = find_pixel_conv(labeled_image, x - 1, y - 1, 0); // top left
 
 				if(top_pixel > 1 && left_pixel > 1 && top_pixel != left_pixel){
-					printf("gang\n");
 					if(top_pixel < left_pixel){
 						current_pixel = top_pixel;
-						// updates list?
-						update_list(&tail, left_pixel, top_pixel);
+						// change left pixel to top pixel equivalence
+						update_list(tail, left_pixel, top_pixel);
 					} else{
 						current_pixel = left_pixel;
-						// updates list?
-						update_list(&tail, top_pixel, left_pixel);
+						// change top pixel to left pixel equivalence
+						update_list(tail, top_pixel, left_pixel);
 					}
 				}
 
@@ -153,7 +224,6 @@ image raster_scan(image filtered_image, int is_binary){
 				// found new region; top and left pixels are not foreground
 				else{
 					current_pixel = current_equivalence;
-//					printf("current_pixel = %f\n", current_pixel);
 					if(current_equivalence == 2){ // this IF might be redundant
 						node *n = new_node(current_equivalence, current_equivalence);
 						insert_end(&head, &tail, n);
@@ -168,28 +238,30 @@ image raster_scan(image filtered_image, int is_binary){
 			}
 			// sets the labeled image to the region label value
 			labeled_image.data[y * labeled_image.w + x] = current_pixel;
-//			printf("labeled_image.data[y * labeled_image.w + x] = %f\n", labeled_image.data[y * labeled_image.w + x]);
 		}
-//		print_image(labeled_image);
 	}
 
-	printf("Displaying E-List:\n");
-	display_forward(head);
+	list_len = len(head);
+	int s = 2;  // since eq list started at 2, we have to look for 2 + i
 
+	// correct the equivalence list
+	for(int i = 0; i < list_len; i++){
+		temp = search(head, s);
+		s++;
+		node_data = temp->data;
+		node_eq = temp->equivalent;
+		if(node_data != node_eq){
+			float updated_value = get_eq_value_at_index(head, node_eq);
+			update_list(tail, node_data, updated_value);
+		}
+	}
+
+	int regions = get_regions(head);
+	printf("REGIONS = %d\n", regions);
+//	display_forward(head);
+	clean(&head);
+	// MAKE ALL REGIONS BLACK AGAIN; OUTPUT RN IS WEIRD
 	return labeled_image;
-}
-
-/*
- * go through equivalence list against the labeled image and
- * update any connected regions
- *
- * returns segmented_image
- */
-image apply_equivalence_list(image labeled_image, int equivalence_list){
-	// just temporary to get rid of stupid warning
-	image segmented_image = make_image(labeled_image.w, labeled_image.h, 1);
-
-	return segmented_image;
 }
 
 /*
@@ -197,7 +269,7 @@ image apply_equivalence_list(image labeled_image, int equivalence_list){
  *
  * does raster scan and assigns segment labels to foreground
  *
- * returns segmented_image (labeled_image)
+ * returns segmented_image (labeled_image); we might also want to return an array of images with each char in an image
  */
 image segment_image(image filtered_image, int is_binary){
 
